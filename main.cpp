3117b96b4faedb9f38742ec90f1c33ef3958ca0f
johtela <tommi.johtela@dnainternet.net>
2017-12-18 23:24:04 +0200

    Added skeleton to create a font map.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,51 @@
 #include "msdfgen.h"
 #include "msdfgen-ext.h"
 
+#ifdef _WIN32
+#pragma warning(disable:4996)
+#endif
+
 using namespace msdfgen;
 
-int main() {
+typedef Bitmap<FloatRGB>* BITMAP;
+
+BITMAP createCharBitmap(FontHandle *font, char character)
+{
+	Shape shape;
+	if (loadGlyph(shape, font, character)) {
+		shape.normalize();
+		edgeColoringSimple(shape, 3.0); // max. angle
+		BITMAP msdf = new Bitmap<FloatRGB>(32, 32); // image width, height 
+		generateMSDF(*msdf, shape, 4.0, 1.0, Vector2(4.0, 4.0)); // range, scale, translation
+		return msdf;
+	}
+	return NULL;
+}
+
+int main(int argc, const char* const *argv) {
+	if (argc != 3)
+		printf("Usage: msdfgen <fontpath> <charmap>");
+	const char* fontpath = argv[1];
+	const char* charmap = argv[2];
+
 	FreetypeHandle *ft = initializeFreetype();
 	if (ft) {
-		FontHandle *font = loadFont(ft, "C:\\Windows\\Fonts\\arialbd.ttf");
+		FontHandle *font = loadFont(ft, fontpath);
 		if (font) {
-			Shape shape;
-			if (loadGlyph(shape, font, 'A')) {
-				shape.normalize();
-				//                      max. angle
-				edgeColoringSimple(shape, 3.0);
-				//           image width, height
-				Bitmap<FloatRGB> msdf(32, 32);
-				//                     range, scale, translation
-				generateMSDF(msdf, shape, 4.0, 1.0, Vector2(4.0, 4.0));
-				savePng(msdf, "output.png");
+			FILE *cmapFile = fopen(charmap, "r");
+			if (cmapFile) {
+				std::vector<BITMAP> bitmaps;
+				char character;
+				while ((character = fgetc(cmapFile)) >= 0) {
+					BITMAP bitmap = createCharBitmap(font, character);
+					if (bitmap)
+						bitmaps.push_back(bitmap);
+					else
+						break;
+				}
+				for (int i = 0; i < bitmaps.size(); i++)
+					delete bitmaps[i];
+				fclose(cmapFile);
 			}
 			destroyFont(font);
 		}
